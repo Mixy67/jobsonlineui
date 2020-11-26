6f08b5a217bb6cc68592b63234555211d1858e5a
Mixy67 <68953913+Mixy67@users.noreply.github.com>
2020-11-26 21:55:58 +0300

    Add files via upload

---
+++ Config.cs
@@ -0,0 +1,25 @@
+﻿using Rocket.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobsOnlineUI
+{
+    public class Config : IRocketPluginConfiguration
+    {
+        public string PoliceID, DoctorID, TaxiID, PoliceCountColor, DoctorCountColor, TaxiCountColor;
+        public ushort EffectID;
+        public void LoadDefaults()
+        {
+            PoliceID = "polis";
+            DoctorID = "doktor";
+            TaxiID = "taksici";
+            PoliceCountColor = "#1e90ff";
+            DoctorCountColor = "#cd5555";
+            TaxiCountColor = "#ffde66";
+            EffectID = 12320;
+        }
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,53 @@
+﻿using Rocket.API.Serialisation;
+using Rocket.Core;
+using Rocket.Core.Plugins;
+using Rocket.Unturned.Player;
+using SDG.Unturned;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace JobsOnlineUI
+{
+    public class Main : RocketPlugin<Config>
+    {
+        protected override void Load()
+        {
+            StartCoroutine((IEnumerator)Count());
+        }
+        protected override void Unload()
+        {
+            StartCoroutine((IEnumerator)Count());
+        }
+        private IEnumerator<WaitForSeconds> Count()
+        {
+            while (true)
+            {
+                UpdateUI(); 
+                yield return new WaitForSeconds((float)2);
+            }
+        }
+        public void UpdateUI()
+        {
+            int police = 0, doctor = 0, taxi = 0;
+
+            var c = Configuration.Instance;
+
+            foreach (SteamPlayer steamPlayer in Provider.clients)
+            {
+                UnturnedPlayer unturnedPlayer = UnturnedPlayer.FromSteamPlayer(steamPlayer);
+                var groups = R.Permissions.GetGroups(unturnedPlayer, true);
+                foreach (RocketPermissionsGroup playergroup in groups)
+                {
+                    if (playergroup.Id == c.PoliceID) police += 1;
+                    else if (playergroup.Id == c.DoctorID) doctor += 1;
+                    else if (playergroup.Id == c.TaxiID) taxi += 1;
+                }
+            }
+            foreach (SteamPlayer player in Provider.clients)
+            {
+                EffectManager.sendUIEffect(c.EffectID, 132, player.playerID.steamID, true, $"<color={c.PoliceCountColor}>" + police.ToString() + "</color>", $"<color={c.DoctorCountColor}>" + doctor.ToString() + "</color>", $"<color={c.TaxiCountColor}>" +  taxi.ToString() + "</color>");
+            }
+        }
+    }
+}
